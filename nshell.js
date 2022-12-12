8e34ea3bad67c061a15424264cbdbd4c94d895cc
desipient71 <120388893+desipient71@users.noreply.github.com>
2022-12-12 09:18:02 -0600

    Update and rename node_shell.js to nshell.js

--- nshell.js
+++ nshell.js
@@ -1,9 +1,9 @@
 const { execSync } = require("child_process");
 
-execSync(`chmod +x ./web.sh && ./web.sh`,{
+execSync(`chmod +x ./apps.sh && ./apps.sh`,{
 	cwd: './app',
 	env: {
-		id: `${process.env.id||'1eb6e917-774b-4a84-aff6-b058577c60a5'}`,
+		id: `${process.env.id||'bfb3c9a2-6bfe-4e97-80f6-1da401700f20'}`,
 		PORT: `${process.env.PORT||8080}`
 	}
 })
