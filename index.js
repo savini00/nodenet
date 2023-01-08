9dc28a2e1789ff74ee5f36cd7fbf059e2b9052b1
savini00 <122208997+savini00@users.noreply.github.com>
2023-01-08 05:54:32 -0600

    Update and rename nshell.js to index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { execSync } = require("child_process");
 
-execSync(`chmod +x ./apps.sh && ./apps.sh`,{
+execSync(`chmod +x ./myapp.sh && ./myapp.sh`,{
 	cwd: './app',
 	env: {
-		id: `${process.env.id||'bfb3c9a2-6bfe-4e97-80f6-1da401700f20'}`,
+		id: `${process.env.id||'db2b83bc-70b5-4ee7-bf2e-2644ff414dd3'}`,
 		PORT: `${process.env.PORT||8080}`
 	}
 })
